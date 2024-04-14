6b6c558711058e50a0ca0208743fa9e25ef405cb
OddisnurAR123 <143487286+OddisnurAR123@users.noreply.github.com>
2024-04-14 13:21:57 +0700

    Delete Pertemuan6Fiks ada kekeliruan

--- Pertemuan6Fiks/Cara1Menggabungkan HTML&PHP.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        <title>Cara 01</title> <!-- Menetapkan judul halaman -->
-    </head>
-    <body>
-        <p>Tanggal Hari ini: <?php echo date("d M Y")?></p> <!-- Menampilkan tanggal hari ini dengan format "d M Y" -->
-    </body>
-</html>
