8a9fff78afb8d885c430bb6daf32ed048473919f
OddisnurAR123 <143487286+OddisnurAR123@users.noreply.github.com>
2024-04-14 13:12:53 +0700

    Delete Pertemuan6 karena belum fiks

--- Pertemuan6/Cara2Menggabungkan HTML&PHP.php.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo'<html>'; // Membuka tag HTML
-echo'<head><title>Cara02</title></head>'; // Menetapkan judul halaman
-echo'<body>'; // Membuka bagian body dari halaman
-echo'<p>Tanggal Hari ini : '.date('d M Y').'</p>'; // Menampilkan tanggal hari ini dengan format "d M Y"
-echo'</body>'; // Menutup bagian body dari halaman
-echo'</html>'; // Menutup tag HTML
-?>
